bbaccad20c39abe2773df8e83820a2c1fac3a2aa
Morten Bock <morten@mortenbock.dk>
2022-03-25 16:14:43 +0100

    Make pr compile

--- src/Vertica.Umbraco.Headless.Core/Rendering/ContentElementBuilder.cs
+++ src/Vertica.Umbraco.Headless.Core/Rendering/ContentElementBuilder.cs
@@ -35,7 +35,7 @@ namespace Vertica.Umbraco.Headless.Core.Rendering
 
         public virtual ContentElementWithSettings ContentElementWithSettingsFor(IPublishedElement content, IPublishedElement settings)
         {
-		This is not code
+		var test = "this is valid";
 	        var contentElementWithSettings = ContentElementFor<ContentElementWithSettings>(content);
 	        contentElementWithSettings.Settings = ContentElementFor<ContentElement>(settings);
 	        return contentElementWithSettings;
