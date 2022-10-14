7c3231db61aa9bd730af581f155733d2507c35c2
Lars Nikander Frandsen <lnf@vertica.dk>
2022-10-14 07:52:50 +0200

    changed to forms in plural for UmbracoBuilder extension

--- src/Vertica.Umbraco.Headless.Forms/Extensions/UmbracoBuilderExtensions.cs
+++ src/Vertica.Umbraco.Headless.Forms/Extensions/UmbracoBuilderExtensions.cs
@@ -5,7 +5,7 @@ using Vertica.Umbraco.Headless.Forms.Services;
 namespace Vertica.Umbraco.Headless.Forms.Extensions;
 public static class UmbracoBuilderExtensions
 {
-    public static IUmbracoBuilder AddHeadlessForm(this IUmbracoBuilder builder)
+    public static IUmbracoBuilder AddHeadlessForms(this IUmbracoBuilder builder)
     {
         builder.Services.AddSingleton<IHeadlessFormService, HeadlessFormService>();
         return builder;
