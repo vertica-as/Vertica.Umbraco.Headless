2d52591b3f7e9036df4ceed44083ec6eb3316aa9
Morten Bock <mbs@vertica.dk>
2023-08-30 11:05:49 +0200

    fix: Check source argument for null

--- src/Vertica.Umbraco.Headless.Core/Extensions/EnumerableExtensions.cs
+++ src/Vertica.Umbraco.Headless.Core/Extensions/EnumerableExtensions.cs
@@ -6,10 +6,12 @@ namespace Vertica.Umbraco.Headless.Core.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static async Task<TResult[]> ToArrayAsync<T, TResult>(this IEnumerable<T> collection, Func<T, Task<TResult>> func)
+        public static async Task<TResult[]> ToArrayAsync<T, TResult>(this IEnumerable<T> source, Func<T, Task<TResult>> func)
         {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
             var list = new List<TResult>();
-            foreach (var item in collection)
+            foreach (var item in source)
             {
                 var result = await func(item);
                 list.Add(result);
